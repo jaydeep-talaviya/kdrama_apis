483ef98e28a49b7cc08bd58c2b518987ac3e3f2a
jaydeep <jaydeeptalaviya8@gmail.com>
2024-12-11 21:59:33 +0530

    fixed issue for single person get api

--- app/routers/person.py
+++ app/routers/person.py
@@ -5,7 +5,7 @@ from app.dependencies.mongo import get_mongo_db
 from pymongo import ASCENDING,DESCENDING
 from typing import Optional,List
 from bson import ObjectId
-
+from datetime import datetime
 db=get_mongo_db()
 
 person_router = APIRouter(
@@ -29,26 +29,60 @@ def get_all_persons(limit: int = Query(10, gt=0),
                     order_by: Optional[str] = Query("birth_of_date"),  
                     direction: Optional[str] = Query("desc"),
                     jobs: Optional[List[str]] = Query(None),
+                    gender: Optional[str] = Query(None)
+                    # start_year: Optional[int] = Query(None),  # Start year filter for birth
+                    # end_year: Optional[int] = Query(None)     # End year filter for birth
                     ):
-    query = {}
+    # Define the initial pipeline
+    pipeline = []
+    
+    # Search query filtering
     if search:
-        query = {
+        search_query = {
             "$or": [
                 {"tv_channel": {"$regex": search, "$options": "i"}},
                 {"name": {"$regex": search, "$options": "i"}},
-                {"jobs": {"$regex": search, "$options": "i"}}
+                {"jobs": {"$regex": search, "$options": "i"}},
+                {"other_names": {"$regex": search, "$options": "i"}}
             ]
         }
+        pipeline.append({"$match": search_query})
+    
     # Jobs filter using $in operator
     if jobs:
-        query["jobs"] = {"$in": jobs}
+        pipeline.append({"$match": {"jobs": {"$in": jobs}}})
+        pipeline.append({
+            "$addFields": {
+                "matched_jobs_count": {
+                    "$size": {"$setIntersection": ["$jobs", jobs]}
+                }
+            }
+        })
     
+    # Gender filter
+    if gender:
+        pipeline.append({"$match": {"gender": gender}})
+
+   
+    # Sorting by matched jobs count and additional sort field
     sort_direction = ASCENDING if direction == "asc" else DESCENDING
-    persons = list(db.person.find(query).sort(order_by, sort_direction).limit(limit).skip(offset))
-    
+    pipeline.append({
+        "$sort": {
+            "matched_jobs_count": -1,
+            order_by: sort_direction
+        }
+    })
+
+    # Pagination (limit and skip)
+    pipeline.append({"$skip": offset})
+    pipeline.append({"$limit": limit})
+
+    # Perform the aggregation pipeline query
+    persons = list(db.person.aggregate(pipeline))
+        
+    # Process the results and fetch images
     for person in persons:
         person["_id"] = str(person["_id"])
-        # Fetch the person's image from person_images collection
         person_image_doc = db.person_images.find_one({"person_id": ObjectId(person["_id"])})
 
         if person_image_doc and "image_links" in person_image_doc:
@@ -57,7 +91,10 @@ def get_all_persons(limit: int = Query(10, gt=0),
     if not persons:
         raise HTTPException(status_code=404, detail="No Actor/Actress found")
     
-    return {"data": persons, "total_count": db.person.count_documents(query)}
+    # Get the total count for pagination
+    total_count = db.person.count_documents(search_query if search else {})
+    
+    return {"data": persons, "total_count": total_count}
 
 
 
@@ -67,54 +104,77 @@ def get_structure_for_person(collection_name,extra_infos,single_person_id=None):
     for extra_info in extra_infos:
         #movies
         movie_drama = collection.find_one({'_id':extra_info[collection_name+'_id']},{"_id":0,collection_name+'_link':0,"last_paragraph":0,'tv_channel_id':0})
-        extra_info['genres'] = list(db.genre.find({'_id':{"$in":extra_info['genres']}},{'_id':0}))
-        extra_info[collection_name+'_id'] = str(extra_info[collection_name+'_id'])
+        if movie_drama.get('tv_channel'):
+            movie_drama.pop('tv_channel')
+        if extra_info.get('genres'):
+            extra_info['genres'] = list(db.genre.find({'_id':{"$in":extra_info['genres']}},{'_id':0}))
+       
+        if extra_info.get(collection_name+'_id'):
+            extra_info[collection_name+'_id'] = str(extra_info[collection_name+'_id'])
         if single_person_id:
-            movie_drama['name_in_'+collection_name] = db.cast_of_drama.find_one({'$and':[{'cast_id':single_person_id},{'_id':{'$in':extra_info['casts_ids']+extra_info['other_cast_info']}}]},{'cast_name_in_drama':1,"extended_cast":1,"_id":0})
-            del extra_info['casts_ids']
-            del extra_info['other_cast_info']
+            movie_drama['name_in_'+collection_name] = db.cast_of_drama.find_one({'$and':[{'cast_id':single_person_id},{'_id':{'$in':extra_info.get('casts_ids',[])+extra_info.get('other_cast_info',[])}}]},{'cast_name_in_drama':1,"extended_cast":1,"_id":0})
+            if extra_info.get('casts_ids'):
+                del extra_info['casts_ids'] 
+            if extra_info.get('other_cast_info'):
+                del extra_info['other_cast_info']
+        print(">>>>>>>>>>extra_info",movie_drama)
 
         extra_info[collection_name] = movie_drama
 
     return extra_infos
 
 @person_router.get("/{person_id}")
-def get_person_by_id(person_id:str):
-    single_person = db.person.find_one({'_id':ObjectId(person_id)})
+def get_person_by_id(person_id: str):
+    # Find the person by ID
+    single_person = db.person.find_one({'_id': ObjectId(person_id)})
     if not single_person:
         raise HTTPException(status_code=404, detail="No Actor/Actress found")
-    
-    movie_fields = {"movie_id":1,"genres":1,"_id":0}
-    drama_fields = {"drama_id":1,"genres":1,"_id":0}
 
-    directed_by_movies = list(db.movie_extra_info.find({'directed_bys':{"$in":[ObjectId(person_id)]}},movie_fields))
-    directed_by_dramas = list(db.drama_extra_info.find({'directed_bys':{"$in":[ObjectId(person_id)]}},drama_fields))
-    
-    written_of_movies = list(db.movie_extra_info.find({'written_bys':{"$in":[ObjectId(person_id)]}},movie_fields))
-    written_of_dramas = list(db.drama_extra_info.find({'written_bys':{"$in":[ObjectId(person_id)]}},drama_fields))
-    
-    # cast of drama
-    cast_of_drama_ids =  list(db.cast_of_drama.find({'cast_id':{"$in":[ObjectId(person_id)]}},{'_id':1}))
+    # Define the fields to retrieve for movies and dramas
+    movie_fields = {"movie_id": 1, "genres": 1, "_id": 0}
+    drama_fields = {"drama_id": 1, "genres": 1, "_id": 0}
+    print(">>>>>>>>")
+    # Directed movies and dramas
+    directed_by_movies = list(db.movie_extra_info.find({'directed_bys': {"$in": [ObjectId(person_id)]}}, movie_fields))
+    directed_by_dramas = list(db.drama_extra_info.find({'directed_bys': {"$in": [ObjectId(person_id)]}}, drama_fields))
 
+    # Written movies and dramas
+    written_of_movies = list(db.movie_extra_info.find({'written_bys': {"$in": [ObjectId(person_id)]}}, movie_fields))
+    written_of_dramas = list(db.drama_extra_info.find({'written_bys': {"$in": [ObjectId(person_id)]}}, drama_fields))
+
+    # Cast of dramas and movies
+    cast_of_drama_ids = list(db.cast_of_drama.find({'cast_id': {"$in": [ObjectId(person_id)]}}, {'_id': 1}))
     cast_of_drama_ids = [cast_of_drama_id['_id'] for cast_of_drama_id in cast_of_drama_ids]
-    cast_of_movies = list(db.movie_extra_info.find({'$or':[{'casts_ids':{"$in":cast_of_drama_ids}},{'other_cast_info':{"$in":cast_of_drama_ids}}]},{**movie_fields,"casts_ids":1,"other_cast_info":1}))
-    cast_of_dramas = list(db.drama_extra_info.find({'$or':[{'casts_ids':{"$in":cast_of_drama_ids}},{'other_cast_info':{"$in":cast_of_drama_ids}}]},{**drama_fields,"casts_ids":1,"other_cast_info":1}))
+    cast_of_movies = list(db.movie_extra_info.find(
+        {'$or': [{'casts_ids': {"$in": cast_of_drama_ids}}, {'other_cast_info': {"$in": cast_of_drama_ids}}]},
+        {**movie_fields, "casts_ids": 1, "other_cast_info": 1}))
+    cast_of_dramas = list(db.drama_extra_info.find(
+        {'$or': [{'casts_ids': {"$in": cast_of_drama_ids}}, {'other_cast_info': {"$in": cast_of_drama_ids}}]},
+        {**drama_fields, "casts_ids": 1, "other_cast_info": 1}))
+    print(">>>>>>>>")
 
+    # Structure data for response
+    directed_by_movies = get_structure_for_person('movie', directed_by_movies)
+    directed_by_dramas = get_structure_for_person('drama', directed_by_dramas)
+    cast_of_movies = get_structure_for_person('movie', cast_of_movies, single_person['_id'])
+    cast_of_dramas = get_structure_for_person('drama', cast_of_dramas, single_person['_id'])
+    written_of_movies = get_structure_for_person('movie', written_of_movies)
+    written_of_dramas = get_structure_for_person('drama', written_of_dramas)
+    print(">>>>>>>>")
 
-    directed_by_movies = get_structure_for_person('movie',directed_by_movies)
-    directed_by_dramas = get_structure_for_person('drama',directed_by_dramas)
-    cast_of_movies = get_structure_for_person('movie',cast_of_movies,single_person['_id'])
-    cast_of_dramas = get_structure_for_person('drama',cast_of_dramas,single_person['_id'])
-    written_of_movies = get_structure_for_person('movie',written_of_movies)
-    written_of_dramas = get_structure_for_person('drama',written_of_dramas)
-
-
+    # Convert person ID to string
     single_person['_id'] = str(single_person['_id'])
-    single_person['directed_by_movies']=directed_by_movies
-    single_person['directed_by_dramas']=directed_by_dramas
-    single_person['written_of_movies']=written_of_movies
-    single_person['written_of_dramas']=written_of_dramas
-    single_person['cast_of_movies']=cast_of_movies
-    single_person['cast_of_dramas']=cast_of_dramas
-        
+    single_person['directed_by_movies'] = directed_by_movies
+    single_person['directed_by_dramas'] = directed_by_dramas
+    single_person['written_of_movies'] = written_of_movies
+    single_person['written_of_dramas'] = written_of_dramas
+    single_person['cast_of_movies'] = cast_of_movies
+    single_person['cast_of_dramas'] = cast_of_dramas
+
+    # Fetch the person's image from person_images collection
+    person_image_doc = db.person_images.find_one({"person_id": ObjectId(person_id)})
+    if person_image_doc and "image_links" in person_image_doc:
+        single_person['person_images'] = person_image_doc['image_links']  # Include all image links
+    print(">>>>>>",single_person)
+
     return single_person
--- app/schemas/person.py
+++ app/schemas/person.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 class PersonSchema(BaseCommon):
     name: str
     gender: str
-    jobs:List[str]
+    jobs:Optional[List[str]]=[]
     other_names:str
     person_image:Optional[str]=None
     birth_of_date:Optional[str]=None
